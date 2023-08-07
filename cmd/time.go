edf61277579bd6072af546cfe9015bbcfe3b2bd4
zhangxiaofeng05 <zhangxiaofeng05@foxmail.com>
2023-08-08 01:14:30 +0800

    feat(time): interval time calc

--- cmd/time.go
+++ cmd/time.go
@@ -4,12 +4,17 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/zhangxiaofeng05/cherry/internal/timer"
 	"log"
+	"time"
 )
 
-var (
-	calculateTime string
-	duration      string
-)
+var startTimeStr, endTimeStr string
+
+func init() {
+	intervalTimeCalcCmd.PersistentFlags().StringVarP(&startTimeStr, "start", "s", "", "Start time in DateOnly format")
+	intervalTimeCalcCmd.PersistentFlags().StringVarP(&endTimeStr, "end", "e", "", "End time in DateOnly format")
+	intervalTimeCalcCmd.MarkPersistentFlagRequired("start")
+	intervalTimeCalcCmd.MarkPersistentFlagRequired("end")
+}
 
 var timeCmd = &cobra.Command{
 	Use:   "time",
@@ -34,6 +39,29 @@ var nowTimeCmd = &cobra.Command{
 	},
 }
 
+var intervalTimeCalcCmd = &cobra.Command{
+	Use:   "interval",
+	Short: "两个时间的间隔",
+	Long:  "两个时间的间隔",
+	Run: func(cmd *cobra.Command, args []string) {
+		startTime, err := time.Parse(time.DateOnly, startTimeStr)
+		if err != nil {
+			log.Fatalf("Error parsing start time err:%v", err)
+		}
+
+		endTime, err := time.Parse(time.DateOnly, endTimeStr)
+		if err != nil {
+			log.Fatalf("Error parsing end time err: %v", err)
+		}
+
+		interval := endTime.Sub(startTime)
+		h := interval.Hours() / 24
+
+		log.Printf("Time Interval: %v day", h)
+	},
+}
+
 func init() {
 	timeCmd.AddCommand(nowTimeCmd)
+	timeCmd.AddCommand(intervalTimeCalcCmd)
 }
